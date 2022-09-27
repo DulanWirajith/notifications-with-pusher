93db3b5381bb84d584aa2002101996ba5a6cbe1a
dulanwirajith <dulanwirajith1995@gmail.com>
2022-09-27 16:55:21 +0530

    notifications done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React, {useState} from 'react';
 import './App.css';
-import {Notifications} from "./notifications";
+import Notifications from './notifications';
 
 function App() {
-    const [userName, setUserName] = useState<string>("");
-
     return (
         <div className="App">
-            <div>
-                Notifications HERE
-            </div>
-            =======================
-            <Notifications></Notifications>
-            {/*<div>*/}
-            {/*    {notifications.map((object, i) => {*/}
-            {/*        console.log(object);*/}
-            {/*        return (<div>*/}
-            {/*            {object}*/}
-            {/*        </div>)*/}
-            {/*    })}*/}
-            {/*</div>*/}
+            <Notifications/>
         </div>
     );
 }
--- src/notifications.tsx
+++ src/notifications.tsx
@@ -1,17 +1,103 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "axios";
 
-export const Notifications = ()=>{
-    const [notifications, setNotifications] = useState<string[]>([]);
+export default function Notifications() {
+  const [notifications, setNotifications] = useState<any[]>([]);
+  const [newNotificationCount, setNewNotificationCount] = useState(0);
+  const userId = "39fd824c-4b63-4de5-97b5-407965d70106";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImUuYW5hZG9sQGdtYWlsLmNvbSIsImlhdCI6MTY2NDI2MTgwMiwiZXhwIjoxNjY2MjcxODAyfQ.iOQc5HskVyC8ZY-YqIhIgjKJLep3CGqg370fB_xiJNE";
 
-    useEffect(()=>{
-        setNotifications(['asdf', 'fda', 'fafda']);
-    },[])
-    return (<div>
-        {notifications.map((object, i) => {
-            console.log(object);
-            return (<div>
-                {object}
-            </div>)
-        })}
-    </div>);
+  const getNotifications = () => {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    axios
+      .get("http://localhost:3333/notifications", config)
+      .then((data: any) => {
+        setNotifications(data.data);
+        let newNotifCount = 0;
+        data.data.map((notif: any) => {
+          if (notif.readAt === null) {
+            newNotifCount++;
+          }
+        });
+        setNewNotificationCount(newNotifCount);
+      })
+      .catch((err) => {
+        console.log("Something went wrong");
+      });
+  };
+
+  useEffect(() => {
+    const pusher = new Pusher(`${process.env.REACT_APP_KEY}`, {
+      cluster: `${process.env.REACT_APP_CLUSTER}`,
+      authEndpoint: "http://localhost:3333/notifications/auth",
+      auth: {
+        params: {
+          userId,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    });
+
+    const channel = pusher.subscribe(`presence-${userId}`);
+    channel.bind("new_notification", (data: any) => {
+      setNotifications(prestate => [data, ...prestate])
+    });
+
+    channel.bind("pusher:subscription_succeeded", (members: any) => {
+      getNotifications(); 
+    });
+
+    return () => {
+      pusher.unsubscribe(`presence-${userId}`);
+    };
+  }, []);
+
+  useEffect(() => {
+    let newNotifCount = 0;
+    notifications.map((notif: any) => {
+      if (notif.readAt === null) {
+        newNotifCount++;
+      }
+    });
+    setNewNotificationCount(newNotifCount);
+  }, [notifications])
+
+
+  return (
+    <div style={{alignItems:"flex-start", display:'flex'}} >
+    <div style={{width: '50%', alignItems:'center'}}>
+      <div>
+      <h2>UNREAD NOTIFICATIONS COUNT = {newNotificationCount}</h2>
+      </div>
+      {notifications.map((object, i) => {
+        return (
+          <div
+            key={object.id}
+            style={{
+              border: "1px solid blue",
+              margin: "8px",
+              backgroundColor: `${
+                object?.readAt === null ? "greenyellow" : null
+              }`,
+            }}
+          >
+            <p>{`title: ${object?.title!} description: ${object?.description!} `}</p>
+            <p>{`isRead: ${object?.readAt === null ? false : true}`}</p>
+          </div>
+        );
+      })}
+    </div>
+    <div style={{height:"500px", display:'flex', alignItems:'center'}}>
+      <h3> * Unread Notificaions are highlighted with green color</h3>
+    </div>
+    </div>
+
+  );
 }
